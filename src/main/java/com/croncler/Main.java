558c9bdf777b1abafe64bd3147fd413935e15f37
Jup0r <Joliansb@gmail.com>
2025-11-27 09:31:09 +0000

    image reading stuff

--- src/main/java/com/croncler/Main.java
+++ src/main/java/com/croncler/Main.java
@@ -1,7 +1,36 @@
 package com.croncler;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        File imageFile = new File("testimg.png");
+        Tesseract instance = new Tesseract();
+
+        Robot robot = new Robot();
+
+        while (true) {
+
+            Thread.sleep(1000);
+            Point mousePos = MouseInfo.getPointerInfo().getLocation();
+
+            int height = 200;
+            int width = 300;
+            Rectangle mouseBox = new Rectangle(mousePos.x - width / 2,mousePos.y - height / 2,width,height);
+
+
+            BufferedImage image = robot.createScreenCapture(mouseBox);
+            try {
+                String result = instance.doOCR(image);
+                System.out.println(result);
+            } catch (TesseractException e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
---
+++ src/main/java/com/croncler/ScreenReader.java
@@ -0,0 +1,47 @@
+package com.croncler;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ScreenReader {
+    private static Robot robot;
+    private static Tesseract instance;
+
+    public static String readAtPos(int x,int y, int width, int height) throws AWTException {
+        Rectangle rect = new Rectangle(x,y,width,height);
+        String result = null;
+        if (robot == null) {
+            robot = new Robot();
+        }
+        if (instance == null) {
+            instance = new Tesseract();
+        }
+        BufferedImage image = robot.createScreenCapture(rect);
+        try {
+            result = instance.doOCR(image);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public static String readAtPos(Rectangle rect) throws AWTException {
+        String result = null;
+        if (robot == null) {
+            robot = new Robot();
+        }
+        if (instance == null) {
+            instance = new Tesseract();
+        }
+        BufferedImage image = robot.createScreenCapture(rect);
+        try {
+            result = instance.doOCR(image);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+}
